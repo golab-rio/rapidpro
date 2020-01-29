faf8c3a7f7757b83f40181971108f0d1c57f7f95
Teeh Amaral <contato@teehamaral.me>
2020-01-29 00:08:06 -0300

    Change the supervisord path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ case $1 in
         python3.6 manage.py compress --extension=.haml --force
         python3.6 clear-compressor-cache.py
         python3.6 manage.py migrate --noinput
-        /usr/local/bin/supervisord -n -c supervisor-app.conf
+        /usr/bin/supervisord -n -c supervisor-app.conf
     ;;
 
 esac
