cc755cad27977c7c728de1ea37d93867183088ec
Teeh Amaral <contato@teehamaral.me>
2020-01-30 00:03:18 -0300

    Some fixes on deploy process

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ case $1 in
         python3.6 manage.py compress --extension=.haml --force
         python3.6 clear-compressor-cache.py
         python3.6 manage.py migrate --noinput
-        /usr/bin/supervisord -n -c supervisor-app.conf
+        cat nginx.conf > /etc/nginx/sites-enabled/nginx.conf
+        /usr/local/bin/supervisord -n -c supervisor-app.conf
     ;;
 
 esac
